225d67924ba7fd616e12898582b2c3afad61a219
songyi <iwasborninautumn@gmail.com>
2023-07-03 23:24:53 +0900

    반응형

--- js/header.js
+++ js/header.js
@@ -1,10 +1,16 @@
 const header = document.querySelector('.header')
 const headerHeight = header.getBoundingClientRect().height
+const logo = document.querySelector('.header .logo')
+const logoSpan = document.querySelector('header .logo span')
 
 window.addEventListener('scroll', () => {
     if (window.scrollY > headerHeight) {
-        header.setAttribute('style', 'background: #ffffff; transition: all 0.3s;')
+        header.setAttribute('style', 'background: #ffffff;')
+        logo.setAttribute('style', 'color: #333333;')
+        logoSpan.setAttribute('style', 'color: #fc6a65;')
     } else {
-        header.setAttribute('style', 'background: transparent; transition: all 0.3s;')
+        header.setAttribute('style', 'background: transparent;')
+        logo.setAttribute('style', 'color: #ffffff;')
+        logoSpan.setAttribute('style', 'color: #0018ff;')
     }
 })
